91892015d79204f5eef9ef60f2e9299df5fb641a
noctsol <39346808+Noctsol@users.noreply.github.com>
2025-09-04 12:18:54 -0700

    Adding files

---
+++ Patches/ElementContainerPatches.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using ExperienceMultiplier.Extensions;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ExperienceMultiplier.Patches;
+
+public class ElementContainerPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ElementContainer), nameof(ElementContainer.ModExp))]
+    public static void ElementContainerModExp_Prefix(ref ElementContainer __instance, int ele, ref float a, bool chain)
+    {
+        var chara = __instance.Chara;
+        var element = __instance.GetElement(ele);
+
+        if (chara == null || !chara.isChara || chara.isDead || element == null) return;
+
+        var __ai = Mathf.RoundToInt(a);
+        a = ResultMultipliedValue(element, __ai, ExperienceMultiplierValueResolver, ResultValueResolver);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ElementContainer), nameof(ElementContainer.ModPotential))]
+    public static void ElementContainerModPotential_Prefix(ref ElementContainer __instance, int ele, ref int v)
+    {
+        var chara = __instance.Chara;
+        var element = __instance.GetElement(ele);
+
+        if (chara == null || !chara.isChara || chara.isDead || element == null) return;
+
+        v = ResultMultipliedValue(element, v, PotentialMultiplierValueResolver, ResultValueResolver);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ElementContainer), nameof(ElementContainer.ModTempPotential))]
+    public static void ElementContainerModTempPotential_Prefix(ref ElementContainer __instance, int ele, ref int v, int threshMsg = 0)
+    {
+        var chara = __instance.Chara;
+        var element = __instance.GetElement(ele);
+
+        if (chara == null || !chara.isChara || chara.isDead || element == null) return;
+
+        v = ResultMultipliedValue(element, v, PotentialMultiplierValueResolver, ResultValueResolver);
+    }
+
+    public static int PotentialLossValue(Element element)
+    {
+        var value = element.vTempPotential / 4 + EClass.rnd(5) + 5;
+        return ResultMultipliedValue(element, value, PotentialLossMultiplierValueResolver, ResultValueResolver);
+    }
+
+    private static int ResultMultipliedValue(Element element, int value, Func<Element, int, int> multiplierResolver, Func<Chara, int, int, int> resultValueResolver)
+    {
+        var chara = element.owner?.Chara;
+        if (chara == null || !chara.isChara || chara.isDead) return value;
+
+        var multipliedValue = multiplierResolver(element, value);
+        var resultValue = resultValueResolver(chara, value, multipliedValue);
+
+        return resultValue;
+    }
+
+    private static int ResultValueResolver(Chara chara, int value, int multipliedValue)
+    {
+        var resultValue = value;
+
+        if (PluginSettings.MainCharacter.Value)
+        {
+            if (chara.IsPC)
+                resultValue = multipliedValue;
+        }
+
+        if (PluginSettings.Faction.Value)
+        {
+            if (chara.IsPCFaction && !chara.IsPC && !chara.IsPCParty)
+                resultValue = multipliedValue;
+        }
+
+        if (PluginSettings.Party.Value)
+        {
+            if (chara.IsPCParty && !chara.IsPC)
+                resultValue = multipliedValue;
+        }
+
+        if (PluginSettings.PlayerMinion.Value)
+        {
+            if (chara.IsPCMinion())
+                resultValue = multipliedValue;
+        }
+
+        if (PluginSettings.FactionMinion.Value)
+        {
+            if (chara.IsPCFactionMinion && !chara.IsPCMinion() && !chara.IsPCPartyMinion)
+                resultValue = multipliedValue;
+        }
+
+        if (PluginSettings.PartyMinion.Value)
+        {
+            if (chara.IsPCPartyMinion && !chara.IsPCMinion())
+                resultValue = multipliedValue;
+        }
+
+        return resultValue;
+    }
+
+    private static int ExperienceMultiplierValueResolver(Element element, int value)
+    {
+        var multiplier = 1.0f;
+
+        if (element.IsMainAttribute())
+            multiplier = PluginSettings.ExperienceMultiplierMainAttributes.Value;
+
+        if (element.IsSkillWeapon())
+            multiplier = PluginSettings.ExperienceMultiplierSkillWeapon.Value;
+
+        if (element.IsSkillCombat())
+            multiplier = PluginSettings.ExperienceMultiplierSkillCombat.Value;
+
+        if (element.IsSkillCraft())
+            multiplier = PluginSettings.ExperienceMultiplierSkillCraft.Value;
+
+        if (element.IsSkillGeneral())
+            multiplier = PluginSettings.ExperienceMultiplierSkillGeneral.Value;
+
+        if (element.IsSkillMind())
+            multiplier = PluginSettings.ExperienceMultiplierSkillMind.Value;
+
+        if (element.IsSkillStealth())
+            multiplier = PluginSettings.ExperienceMultiplierSkillStealth.Value;
+
+        if (element.IsSkillLabor())
+            multiplier = PluginSettings.ExperienceMultiplierSkillLabor.Value;
+
+        if (element.IsSpellAbility())
+            multiplier = PluginSettings.ExperienceMultiplierSpellAbility.Value;
+
+        return Mathf.RoundToInt(value * multiplier);
+    }
+
+    private static int PotentialMultiplierValueResolver(Element element, int value)
+    {
+        var multiplier = 1.0f;
+
+        if (element.IsMainAttribute())
+            multiplier = PluginSettings.PotentialMultiplierMainAttributes.Value;
+
+        if (element.IsSkillWeapon())
+            multiplier = PluginSettings.PotentialMultiplierSkillWeapon.Value;
+
+        if (element.IsSkillCombat())
+            multiplier = PluginSettings.PotentialMultiplierSkillCombat.Value;
+
+        if (element.IsSkillCraft())
+            multiplier = PluginSettings.PotentialMultiplierSkillCraft.Value;
+
+        if (element.IsSkillGeneral())
+            multiplier = PluginSettings.PotentialMultiplierSkillGeneral.Value;
+
+        if (element.IsSkillMind())
+            multiplier = PluginSettings.PotentialMultiplierSkillMind.Value;
+
+        if (element.IsSkillStealth())
+            multiplier = PluginSettings.PotentialMultiplierSkillStealth.Value;
+
+        if (element.IsSkillLabor())
+            multiplier = PluginSettings.PotentialMultiplierSkillLabor.Value;
+
+        if (element.IsSpellAbility())
+            multiplier = PluginSettings.PotentialMultiplierSpellAbility.Value;
+
+        return Mathf.RoundToInt(value * multiplier);
+    }
+
+    private static int PotentialLossMultiplierValueResolver(Element element, int value)
+    {
+        var multiplier = 1.0f;
+
+        if (element.IsMainAttribute())
+            multiplier = PluginSettings.PotentialLossMultiplierMainAttributes.Value;
+
+        if (element.IsSkillWeapon())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillWeapon.Value;
+
+        if (element.IsSkillCombat())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillCombat.Value;
+
+        if (element.IsSkillCraft())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillCraft.Value;
+
+        if (element.IsSkillGeneral())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillGeneral.Value;
+
+        if (element.IsSkillMind())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillMind.Value;
+
+        if (element.IsSkillStealth())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillStealth.Value;
+
+        if (element.IsSkillLabor())
+            multiplier = PluginSettings.PotentialLossMultiplierSkillLabor.Value;
+
+        if (element.IsSpellAbility())
+            multiplier = PluginSettings.PotentialLossMultiplierSpellAbility.Value;
+
+        return Mathf.RoundToInt(value * multiplier);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # moarexp
-Elin game experience mod that spawned from another deprecated one
+Elin game experience mod that spawned from another deprecated one (Experience Multiplier). 
+
+
+
+# Notes
+First time ever modding anything so will leave this in case anyone picks this up.
+This took me longer than I'd like to admit for something so simple.
+I NEEDED this mod to take the game reasonable to play. All I wanted was a reasonable exp rate lol.
+
+In the future, the experience functionality might change again. I changed things to be less fragile by patching the method rather than in-line replacements.
+What originally broke the original mod was that the ModExp function changed from using an int to a float.
+
+1. Download dns spy
+2. Go to your elin installation (mine was at D:\SteamLibrary\steamapps\common\Elin)
+3. In dnsspy, open up D:\SteamLibrary\steamapps\common\Elin\Elin_Data\Managed\elin.dll
+    - Look for "ModExp"
+    - If it doesn't exist, prepare to spend a lot of time digging and writing entirely new code
+
+
+# Guide for running this locally
+## Option 1 - Drop it in the package directory
+Example: D:\SteamLibrary\steamapps\common\Elin\Package
+1. Make a folder called /tempthing in the ..SteamLibrary\steamapps\common\Elin\Package directory
+Run `dotnet build -c Release` to make a dll
+2. Drop the dll in D:\SteamLibrary\steamapps\common\Elin\BepInEx\plugins
+4. Run the game once and a config file will get generated
+5. Close the game
+6. Edit the file
+
+## Option 2 - Edit the csproj pathing 
+1. Edit the csproj pathing to reference your files
+2. Run `dotnet build -c Release` to make a dll
+3. Drop the dll in D:\SteamLibrary\steamapps\common\Elin\BepInEx\plugins
+4. Close the game
+5. Edit the file
